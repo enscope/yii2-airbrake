3cde99cc81f5231a280954ebf6c4e0a1e019cf8c
Tachi <25849492+feder98@users.noreply.github.com>
2023-07-24 14:41:44 -0300

    Nested params are masked.

--- src/ParamsFilter.php
+++ src/ParamsFilter.php
@@ -27,8 +27,13 @@ class ParamsFilter extends BaseObject
 
         $this->paramsFilter = function ($notice) {
             foreach ($this->params as $param) {
-                if (isset($notice['params'][$param])) {
-                    $notice['params'][$param] = $this->replacement;
+                $paramsToReplace = $this->explodeParams($param);
+
+                if ((count($paramsToReplace) === 1) && isset($notice['params'][$paramsToReplace[0]])) {
+                    $notice['params'][$paramsToReplace[0]] = $this->replacement;
+                }
+                if ((count($paramsToReplace) === 2) && isset($notice['params'][$paramsToReplace[0]][$paramsToReplace[1]])) {
+                    $notice['params'][$paramsToReplace[0]][$paramsToReplace[1]] = $this->replacement;
                 }
             }
             return $notice;
@@ -43,6 +48,22 @@ class ParamsFilter extends BaseObject
     public function getParamsFilter()
     {
         return $this->paramsFilter;
+    }
+
+    /**
+     * @param string $param
+     * @return string[]
+     */
+    private function explodeParams(string $param): array
+    {
+        $superglobals = ['_GET.', '_POST.', '_COOKIE.', '_SERVER.', '_ENV.', '_FILES.'];
+        foreach ($superglobals as $superglobal) {
+            if (strpos($param, $superglobal) === 0) {
+                $param = str_replace($superglobal, "", $param);
+                return explode(".", $param);
+            }
+        }
+        return [$param];
     }
 
 }
